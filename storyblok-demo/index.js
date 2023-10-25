78ac8a5f625f62e9877521ab07c66b39dcd2aacc
Ben Giannis <benjamingiannis@gmail.com>
2023-10-25 14:52:49 -0400

    Storyblok test

---
+++ storyblok-demo/index.js
@@ -0,0 +1,15 @@
+fetch("https://api-us.storyblok.com/v2/cdn/stories/bengiannis?token=Fm5wlsy8rzc1dPxqi1rs9gtt")
+  .then(response => response.json())
+  .then(data => {
+    const bodyContent = data.story.content.body;
+    const mainElement = document.querySelector("main");
+    
+    bodyContent.forEach(item => {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = item.Text;
+      mainElement.appendChild(paragraph);
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
