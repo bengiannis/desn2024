64d0552fcf3be0619f5875298973b86de9ee2ad8
Ben Giannis <benjamingiannis@gmail.com>
2024-04-17 14:48:30 -0400

    Updated automation

--- netlify/functions/webflow-storyblock-automation.js
+++ netlify/functions/webflow-storyblock-automation.js
@@ -22,13 +22,31 @@ export default async (req, context) => {
     return;
   }
 
-  const fetchExistingCollectionItems = await fetch("https://api.webflow.com/v2/collections/65a3037472b070dda83f4b2d/items", {
-    method: 'GET',
-    headers: headers
-  });
-  const existingCollectionItems = await fetchExistingCollectionItems.json();
+  const baseUrl = "https://api.webflow.com/v2/collections/65a3037472b070dda83f4b2d/items";
+  let offset = 0;
+  let total = null;
+  let allItems = [];
 
-  if (existingCollectionItems.items.some(item => item.fieldData.slug == storySlug)) {
+  do {
+    const url = `${baseUrl}?limit=100&offset=${offset}`;
+    const fetchResponse = await fetch(url, {
+      method: 'GET',
+      headers: headers
+    });
+    const responseJson = await fetchResponse.json();
+    allItems = allItems.concat(responseJson.items);
+
+    // Update the offset for the next iteration
+    offset += responseJson.items.length;
+
+    // Set total from the response if it's not already set
+    if (total === null) {
+      total = responseJson.pagination.total;
+    }
+  } while (offset < total);
+
+  // Check if any item matches the storySlug
+  if (allItems.some(item => item.fieldData.slug === storySlug)) {
     return;
   }
 
